765f67e11ead4788f8d42c0df53edc05d705b0e9
Nils Gawlik <nilsgawlik@gmx.de>
2018-11-14 15:27:43 +0100

    Add enemy dummy and weird door

---
+++ ReplaceDungeonGenerator/Assets/Game/Scripts/BasicEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		Rigidbody rb = GetComponent<Rigidbody>();
+		rb.constraints = RigidbodyConstraints.None;
+	}
+}
