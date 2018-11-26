3054e014b0bb61c9e4f0f43f96152658d03f7c24
Nils Gawlik <nilsgawlik@gmx.de>
2018-11-26 23:51:57 +0100

    Add directionality and expansion rules

---
+++ ReplaceDungeonGenerator/Assets/Game/Scripts/ArrowGizmo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowGizmo : MonoBehaviour {
+	public Mesh arrowShaftMesh;
+	public Mesh arrowHeadMesh;
+	public Color color = Color.white;
+
+	public int shaftRot = 0;
+	public int headRot = 0;
+
+	private void OnDrawGizmos() {
+		Gizmos.color = color;
+		if(arrowShaftMesh) Gizmos.DrawMesh(
+			arrowShaftMesh, 
+			-1, 
+			transform.position, 
+			transform.rotation * Quaternion.Euler(0, shaftRot * 90, 0), 
+			transform.localScale
+		);
+		
+		if(arrowHeadMesh) Gizmos.DrawMesh(arrowHeadMesh, 
+			-1, 
+			transform.position, 
+			transform.rotation * Quaternion.Euler(0, headRot * 90, 0), 
+			transform.localScale
+		);
+	}
+}
