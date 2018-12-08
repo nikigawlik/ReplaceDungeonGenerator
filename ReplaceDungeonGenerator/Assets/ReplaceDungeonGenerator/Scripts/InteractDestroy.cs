286b10661797ea22961f7f9fc390608cab2e3348
Nils Gawlik <nilsgawlik@gmx.de>
2018-12-08 19:43:06 +0100

    Add one way obstacle

--- ReplaceDungeonGenerator/Assets/ReplaceDungeonGenerator/Scripts/InteractDestroy.cs
+++ ReplaceDungeonGenerator/Assets/ReplaceDungeonGenerator/Scripts/InteractDestroy.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 [RequireComponent(typeof(Interactible))]
 public class InteractDestroy : MonoBehaviour {
+	public GameObject objectToDestroy;
+
 	void Start () {
 		Interactible i = GetComponent<Interactible>();
 		i.onInteract.AddListener(Destruct);
 	}
 	
 	public void Destruct() {
-		GameObject.Destroy(gameObject);
+		objectToDestroy = objectToDestroy? objectToDestroy : gameObject;
+		GameObject.Destroy(objectToDestroy);
 	}
 }
